9d20762ab3ec503cd6fd8b3ab1cbb7b36755ed13
Username <codrinaignat@hotmail.com>
2023-09-23 14:10:28 +0300

    Solve problem 5

--- Final_exercise_Lisaru.py
+++ Final_exercise_Lisaru.py
@@ -1,10 +1,10 @@
-# 1. Ex1 starts from here
+# 1. Open and read the information from Database.txt
 
 import re
 input = open('Database.txt', 'r')
 fileContent = input.read()
 
-# 2. Ex2 starts from here
+# 2. Create 5 lists for each basic function (the prefix of the Parameter_name value indicates the basic function PpDae – DAE (DriverActivityEstimation), Ada – ADA (ActionDecisionAsil), PpRpa – RPA (ReferencePointAdjustment), PpLv – LV (LaneVerification), Asq – ASQ (ActionSuppressionQM)), where the list elements are the lines from the text file (e.g [„string line1“, „string lineX“])
 
 lines = fileContent.split('\n')
 
@@ -25,15 +25,13 @@ for line in lines:
         LVList.append(line)
     elif 'Asq' in line:
         ASQList.append(line)
-    else:
-        print(' ')
 # print(*DAEList, sep='\n')
 # print(*ADAList, sep='\n')
 # print(*RPAList, sep='\n')
 # print(*LVList, sep='\n')
 # print(*ASQList, sep='\n')
 
-# Ex3 starts from here
+# Ex3 Define a function to create a list of dictionaries where the key and value pairs are obtain from the equality (there should be 6 pairs) (e.g. [{key1:value1, key2:value3}, {key1:value2, key2:value4}])
 
 
 def parameterStringsToDictionaries(stringLines):
@@ -56,20 +54,61 @@ def parameterStringsToDictionaries(stringLines):
         singleDictionary = dict(zip(keys, values))
         listOfDictionaries.append(singleDictionary)
 
-    # print(*listOfDictionaries, sep='\n')
-    # print(listOfDictionaries)
-    return
+    return listOfDictionaries
 
 
-parameterStringsToDictionaries(lines)
+# print(parameterStringsToDictionaries(DAEList))
 
 # 4. With the defined function create a single dictionary having the keys the names of the
 # basic functions and the value the list of dictionaries (previously obtained) (e.g. {RPA:[{}, {}], DAE:[{}, {}]})
 
 
+basicFunctionNames = ['PpDae', 'Ada', 'PpRpa', 'PpLv', 'Asq']
+basicFunctionLists = [DAEList, ADAList, RPAList, LVList, ASQList]
+basicFunctionDictionaries = []
+
+for basicFunctionList in basicFunctionLists:
+    basicFunctionDictionaries.append(
+        parameterStringsToDictionaries(basicFunctionList))
+
+basicFunctions = dict(zip(basicFunctionNames, basicFunctionDictionaries))
+
+# print(basicFunctions, sep="\n")
+
+
 # 5. Using the obtained dictionary, print the Parameter_name value where the key „value“ has array (the type is defined as: type=„type[]“)
 
+PpDaeParameters = basicFunctions["PpDae"]
+# print(PpDaeParameters[1]["type"])
+AdaParameters = basicFunctions["Ada"]
+PpRpaParameters = basicFunctions["PpRpa"]
+PpLvParameters = basicFunctions["PpLv"]
+AsqParameters = basicFunctions["Asq"]
+
+
+def hasArrayType(parameter):
+    return "[]" in parameter["type"]
+
+# print(hasArrayType(PpDaeParameters[1]))
+
+
+def filterArrayTypeParameters(parameters):
+    arrayParameters = []
+    for parameter in parameters:
+        if hasArrayType(parameter):
+            arrayParameters.append(parameter)
+    return arrayParameters
+
+arrayTypeParameters = filterArrayTypeParameters(PpDaeParameters)
+print(arrayTypeParameters)
+
+for parameter in arrayTypeParameters:
+    print(parameter["Parameter_name"])
+
+
 # 6. Using the obtained dictionary, print the Parameter_name where the key „lib:fusi “ is 1
+
+
 
 # 7. Using the obtained dictionary, for the parameters of type Uint16, replace the „dd:MaxRange“ value which is
 # different than the maximum value allowed by the data type (65 535)
