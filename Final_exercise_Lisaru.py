c28511276d571ee66e17a28d3293853526135193
Username <codrinaignat@hotmail.com>
2023-09-23 12:42:37 +0300

    Code and files optimization

--- Final_exercise_Lisaru.py
+++ Final_exercise_Lisaru.py
@@ -1,53 +1,73 @@
-# 1. Open and read the information from Database.txt
+# 1. Ex1 starts from here
 
+import re
 input = open('Database.txt', 'r')
-myString = input.read()
+fileContent = input.read()
 
+# 2. Ex2 starts from here
 
-
-# 2. Create 5 lists for each basic function (the prefix of the Parameter_name value indicates the basic function
-# PpDae – DAE (DriverActivityEstimation),
-# Ada – ADA (ActionDecisionAsil),
-# PpRpa – RPA (ReferencePointAdjustment),
-# PpLv – LV (LaneVerification),
-# Asq – ASQ (ActionSuppressionQM)),
-# where the list elements are the lines from the text file (e.g [„string line1“, „string lineX“])
-
-myBigList = myString.split("<")
-
+myBigList = fileContent.split("<")
 #len(myBigList) returns 71
 
 x = range(71)
-PpDaeList = []
-AdaList = []
-PpRpaList = []
-PpLvList = []
-AsqList = []
-
+DAEList = []
+ADAList = []
+RPAList = []
+LVList = []
+ASQList = []
 
 for n in x:
     if 'PpDae' in myBigList[n]:
-        PpDaeList.append(myBigList[n])
+        DAEList.append(myBigList[n])
     elif 'Ada' in myBigList[n]:
-        AdaList.append(myBigList[n])
+        ADAList.append(myBigList[n])
     elif 'PpRpa' in myBigList[n]:
-        PpRpaList.append(myBigList[n])
+        RPAList.append(myBigList[n])
     elif 'PpLv' in myBigList[n]:
-        PpLvList.append(myBigList[n])
+        LVList.append(myBigList[n])
     elif 'Asq' in myBigList[n]:
-        AsqList.append(myBigList[n])
+        ASQList.append(myBigList[n])
     else:
         print(' ')
+print(*DAEList, sep='\n')
+#print(*ADAList, sep='\n')
+#print(*RPAList, sep='\n')
+#print(*LVList, sep='\n')
+#print(*ASQList, sep='\n')
 
+#Ex3 starts from here
 
-# 3. Define a function to create a list of dictionaries where
-# the key and value pairs are obtain from the equality (there should be 6 pairs)
-# e.g. [{key1:value1, key2:value3}, {key1:value2, key2:value4}])
+lines = fileContent.split('\n')
+def parameterStringsToDictionaries (stringLines):
+    
+    keys = []
+    values = []
+    listOfDictionaries = []
 
-#def CreateDictionariesList ()
+    for line in stringLines:
+        withoutBrackets = line.replace('<', '').replace('/>', '')
+        words = re.split(r'" |="|"', withoutBrackets)
+        words.pop()
+
+        for index, word in enumerate(words):
+            if (index % 2 == 0):
+                keys.append(word)
+            else:
+                values.append(word)
+
+        singleDictionary = dict(zip(keys, values))
+        listOfDictionaries.append(singleDictionary)
+
+    print(*listOfDictionaries, sep='\n')
+    #print(listOfDictionaries)
+    return
+
+parameterStringsToDictionaries(lines)
 
 # 4. With the defined function create a single dictionary having the keys the names of the
 # basic functions and the value the list of dictionaries (previously obtained) (e.g. {RPA:[{}, {}], DAE:[{}, {}]})
+
+
 
 # 5. Using the obtained dictionary, print the Parameter_name value where the key „value“ has array (the type is defined as: type=„type[]“)
 
